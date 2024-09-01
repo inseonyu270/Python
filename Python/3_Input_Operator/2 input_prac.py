69e8bda784ea2673878a4b744df5b997f2330b53
유인선 <124290448+inseonyu270@users.noreply.github.com>
2024-09-01 10:49:03 +0900

    Update 2 input_prac.py

--- Python/3_Input_Operator/2 input_prac.py
+++ Python/3_Input_Operator/2 input_prac.py
@@ -9,5 +9,21 @@
 name = input('이름을 입력하세요 >>> ')
 age = input('나이를 입력하세요 >>> ')
 
+# f-strings
 print(f'입력된 이름은 {name}입니다.')
 print(f'입력된 나이는 {age}살입니다.')
+
+# format
+#print('입력된 이름은 {}입니다.'.format(name))                  // 형식 지정자 {}
+#print('입력된 나이는 {}입니다.'.format(age))
+
+#print('입력된 이름은 {f_name}입니다.'.format(f_name=name))     // 변수 활용
+#print('입력된 나이는 {f-age}입니다.'.format(f_age))            
+
+# %연산자
+#print('입력된 이름은 %s입니다.' %name)
+#print('입력된 나이는 %d입니다.' %age)    
+
+# print() sep옵션
+#print('입력된 이름은 ', name, '입니다.', sep='')
+#print('입력된 나이는 ', age, '입니다.', sep='')
