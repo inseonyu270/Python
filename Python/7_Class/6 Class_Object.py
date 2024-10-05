d70e0340048524496263c8ee376d006edded7e39
유인선 <124290448+inseonyu270@users.noreply.github.com>
2024-10-05 10:41:44 +0900

    Update 6 Class_Object.py

--- Python/7_Class/6 Class_Object.py
+++ Python/7_Class/6 Class_Object.py
@@ -1,16 +1,16 @@
-# 1. 객체의 개념 (object)
-# 객체는 모든 것. 존재하지 않는것도 객체에 해당, 객체는 상속이 가능하다
+# 1. 객체의 개념 (Object)
+# 객체는 모든 것. 존재하지 않는것도 객체에 해당, 객체는 상속이 가능하다.
 # 개발을 하다보면 정수나 실수 또는 문자열 등 기본적인 자료형을 표현하기 힘든 것들이 존재
 # 예를 들어 어떤 게시글을 파이썬으로 표현을 한다면
-# 게시글 번호, 제목, 작성자, 내용, 최초작성일자, 최종수정이라 , 조회수
-# 이를 손쉽게 관리하려면 8개의 항목을 하나의 이름을 가진 객체(object)로 만들어서 사용하는 것이 좋다
+# 게시글 번호, 제목, 작성자, 내용, 최초작성일자, 최종수정일자, 조회수
+# 이를 손쉽게 관리하려면 8개의 항목을 하나의 이름을 가진 객체(Object)로 만들어서 사용하는 것이 좋다.
 
 
 # 2. 클래스의 개념
 # 클래스 class는 객체를 만드는 도구
 # 하나의 클래스를 만들어 두면 그 클래스를 통해서 (동일한 구조를 가진) 여러 개의 객체를 만들 수 있다.
 # 같은 클래스로 만든 객체라 하더라도 객체들은 서로 다른 값을 가질 수 있다.
-# 인스턴스 instancec 역시 클래스를 이용해서 생성한 객체를 가리키는 용어
+# 인스턴스 instance 역시 클래스를 이용해서 생성한 객체를 가리키는 용어
 
 
 # 객체와 인스턴스의 미묘한 차이
@@ -25,12 +25,14 @@
 # 클래스 이름은 Upper Camel Case 규칙을 따름
 # 파이썬은 변수나 함수의 이름을 정할 때 언더바(_)를 이용해 단어를 연결하는 Snake Case 방식을 사용하지만
 # 클래스는 Upper Camel Case 규칙을 따름
-# print + member : printmember 1) print_member 2) printMember 3) PrintMember
+# print + member : printmember  1) print_member 2) printMember 3) PrintMember
 
 
 # 클래스는 다음과 같은 형식으로 정의
 # class 클래스:
-# 코드
+#       코드
+
+
 
 # 4. 객체 생성
 # 클래스가 정의되었다면 다음과 같은 형식으로 객체를 생성
@@ -41,36 +43,50 @@
 # 객체2 = 클래스()
 
 
-# 5. 클래스의 정의와 객체 생성
-class WaffleMachine: #waffleMachine 이라는 클래스 정의
+# 5. 클래스 정의와 객체 생성
+class WaffleMachine: # waffleMachine 이라는 클래스 정의
     pass
 
 
-waffle1 = WaffleMachine()       # WaffleMachine이라는 클래스를 이용해 waffle1이라는 객체 생성
-waffle2 = WaffleMachine()
+waffle1 = WaffleMachine()    #  WaffleMachine이라는 클래스를 이용해 waffle1이라는 객체 생성
+waffle2 = WaffleMachine()   
 
 
 print( waffle1 )
 
-# <__main__.WaffleMachine object at 0x000002199467FDA0 : 메모리의 0x000002199467FDA0번지에
-# 저장된 WaffleMachine의 객체라는 읨
+# <__main__.WaffleMachine object at 0x000002199467FDA0> : 메모리의 0x000002199467FDA0번지에
+# 저장된 WaffleMachine의 객체라는 의미
 print( waffle2 )
+
+
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+hong = Person("홍길동", 20, "대구")
+print(hong.name)
+print(hong.age)
+print(hong.address)
 
 
 class Car:
     def __init__(self, make, model):
         self.make = make
         self.model = model
-    
-    
-    def acclerate(self):
-        print(self.make + "차가 가속합니다")
 
-car_object = Car(makje="hyundai", model="SUV")
+
+    def acclerate(self):
+        print(self.make + "차가 가속합니다.")
+
+car_object = Car(make="hyundai", model="SUV")
 print(car_object.make)
 print(car_object.model)
 
-# car_boject = 인스턴스 : 인스턴스는 클래스로부터 생성된 구체적인 사례를 가리킨다.
+car_object.acclerate()
+
+# car_object = 인스턴스 : 인스턴스는 클래스로부터 생성된 구체적인 사례를 가리킨다.
 
 # 객체 : 클래스에 기반을 두고 있으며, 클래스는 객체를 만들기 위한 템플릿
 # 객체는 속성(데이터)와 메서드(동작)을 가질 수 있다.
