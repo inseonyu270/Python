c01f77c42c47184d75b779ae22621e31739c0aa3
유인선 <124290448+inseonyu270@users.noreply.github.com>
2024-10-06 11:38:12 +0900

    Update 9 Class_Prac.py

--- Python/7_Class/9 Class_Prac.py
+++ Python/7_Class/9 Class_Prac.py
@@ -19,7 +19,16 @@ class Watch:
     def see(self):
         print(f'계산된 시간은 {self.hour}시 {self.minute}분 {self.second}초입니다')
 
-
+# 풀이
+# class Watch:
+#     def What(self):
+#         hms = input('시간을 입력해주세요 >>> ')             # 12:12:31
+#         list = hms.split(':')                             # ["12", "12", "31"]
+#         self.hour = int(list[0])
+#         self.minute = int(list[1])
+#         self.second = int(list[2])
+#     def see(self):
+#         print(f'{self.hour}시 {self.minute}분 {self.second}초입니다.')
 
 
 
